73aa9613e6c771c941c9e5a2847e6865fb3ccc89
yuhui <1161569428@qq.com>
2020-09-03 12:37:39 +0800

    fix

--- packages/apps-config/src/api/spec/crust.ts
+++ packages/apps-config/src/api/spec/crust.ts
@@ -18,36 +18,36 @@ export default {
   IASSig: 'Vec<u8>',
   Identity: {
     pub_key: 'Vec<u8>',
-    code: 'Vec<u8>',
+    code: 'Vec<u8>'
   },
   ISVBody: 'Vec<u8>',
   LookupSource: 'AccountId',
   MerchantInfo: {
     address: 'Vec<u8>',
     storage_price: 'Balance',
-    file_map: 'Vec<(Vec<u8>, Vec<Hash>)>',
+    file_map: 'Vec<(Vec<u8>, Vec<Hash>)>'
   },
   MerchantPunishment: {
     success: 'EraIndex',
     failed: 'EraIndex',
-    value: 'Balance',
+    value: 'Balance'
   },
   MerkleRoot: 'Vec<u8>',
   OrderStatus: {
-    _enum: ['Success', 'Failed', 'Pending'],
+    _enum: ['Success', 'Failed', 'Pending']
   },
   PaymentLedger: {
     total: 'Balance',
     paid: 'Balance',
-    unreserved: 'Balance',
+    unreserved: 'Balance'
   },
   Pledge: {
     total: 'Balance',
-    used: 'Balance',
+    used: 'Balance'
   },
   ReportSlot: 'u64',
   Releases: {
-    _enum: ['V1_0_0', 'V2_0_0'],
+    _enum: ['V1_0_0', 'V2_0_0']
   },
   Status: {
     _enum: ['Free', 'Reserved']
@@ -61,7 +61,7 @@ export default {
     provider: 'AccountId',
     client: 'AccountId',
     amount: 'Balance',
-    order_status: 'OrderStatus',
+    order_status: 'OrderStatus'
   },
   SworkerCert: 'Vec<u8>',
   SworkerCode: 'Vec<u8>',
@@ -72,6 +72,6 @@ export default {
     used: 'u64',
     reserved: 'u64',
     cached_reserved: 'u64',
-    files: 'Vec<(Vec<u8>, u64)>',
+    files: 'Vec<(Vec<u8>, u64)>'
   }
 };
